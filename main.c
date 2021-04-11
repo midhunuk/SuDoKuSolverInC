dfcdcb62018ea2c8b231001e225fc7d9d5ed621b
midhunuk <midhunukumar5@gmail.com>
2021-04-11 19:40:20 +0530

    tested code for file handling

--- main.c
+++ main.c
@@ -1,9 +1,37 @@
 #include<stdio.h>
 
+void CopyFile(FILE* fromFile, FILE* toFile)
+{
+    char reader;
+    while (!feof(fromFile))
+    {
+        fscanf(fromFile, "%c", &reader);
+        fprintf(toFile, "%c", reader);
+    }
+    
+}
+
 int main()
 {
-    printf("Hello world \n");
-    printf("Test");
+    FILE* inputFile;
+    char* fileName = "Input.txt";
+    if((inputFile = fopen(fileName, "r"))== NULL)
+    {
+        printf("File could not be opened %s \n", fileName);
+        perror("Error :");
+        return 1;
+    }
+    FILE* outputFile;
+    char* outputfileName = "Output.txt";
+    if((outputFile = fopen(outputfileName, "w"))== NULL)
+    {
+        printf("File could not be opened %s \n", fileName);
+        perror("Error :");
+        return 1;
+    }
+    CopyFile(inputFile, outputFile);
+    fclose(inputFile);
+    fclose(outputFile);
 
     int a;
     scanf("%d", &a);
