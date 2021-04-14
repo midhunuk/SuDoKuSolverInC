6efb970bdaca41cd690e5a64dfde24cf84226583
midhunuk <midhunukumar5@gmail.com>
2021-04-14 10:11:45 +0530

    Removed redundant code

--- SuDoKu.h
+++ SuDoKu.h
@@ -1,26 +1,7 @@
 #include<stdio.h>
 
-typedef struct 
-{
-    int row;
-    int column;
-    int block;
-    int value;
-} CELL;
-
-
-typedef struct 
-{
-    int rowStart;
-    int columnStart;
-} BLOCK;
-
-void InitializeEmptyBoard();
-
 void AddInitialValues(FILE * );
 
 void Solve();
 
 void PrintBoard(FILE * );
-
-void DeleteBoard();
